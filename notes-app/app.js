e29bd54ea6e8bbc5df6050dabe3fa5fbc6e0865b
Omar Zidan <omarzidan87@outlook.com>
2023-10-30 17:36:26 +0100

    add nodemon package

--- notes-app/app.js
+++ notes-app/app.js
@@ -2,4 +2,4 @@ const chalk = require("chalk");
 const getNotes = require("./notes.js");
 
 const msg = getNotes();
-console.log(chalk.green.inverse.bold("Success!"));
+console.log(chalk.red.inverse.bold("Error!"));
