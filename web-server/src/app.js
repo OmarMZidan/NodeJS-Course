f3389e1da4814fb8215595bec6e9b7c8d7b01614
Omar Zidan <omarzidan87@outlook.com>
2023-11-04 13:30:41 +0100

    add html dynamic templates and css

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,15 +1,18 @@
 const path = require("path");
 const express = require("express");
+const hbs = require("hbs");
 
 const app = express();
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, "../public");
-const viewsPath = path.join(__dirname, "../templates");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
 // Setup handlebars engine and view location
 app.set("view engine", "hbs");
 app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
@@ -17,26 +20,44 @@ app.use(express.static(publicDirectoryPath));
 app.get("", (req, res) => {
   res.render("index", {
     title: "Weather App",
-    name: "Aida Hammad",
+    name: "Omar Zidan",
   });
 });
 
 app.get("/about", (req, res) => {
   res.render("about", {
     title: "About Me",
-    name: "Aida Hammad",
+    name: "Omar Zidan",
   });
 });
 
 app.get("/help", (req, res) => {
   res.render("help", {
+    title: "Help",
     helpText: "This is sime helpful text",
+    name: "Omar Zidan",
   });
 });
 app.get("/weather", (req, res) => {
   res.send({
     forecast: "It is snowing",
     location: "Geneva",
+  });
+});
+
+app.get("/help/*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Omar Zidan",
+    errorMessage: "Help article not found.",
+  });
+});
+
+app.get("*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Omar Zidan",
+    errorMessage: "404 - Page not found!",
   });
 });
 // Start the server and listen on port 3000
