24fd352699aeb3b221e1b540d57dcfadc5ca2262
Omar Zidan <omarzidan87@outlook.com>
2023-11-07 14:22:07 +0100

    use fetch & add search form

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,47 @@
-console.log("Client side javascript file is loaded!");
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+const messageOne = document.querySelector("#message-1");
+const messageTwo = document.querySelector("#message-2");
+
+messageOne.textContent = "";
+messageTwo.textContent = "";
+
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const location = search.value;
+
+  messageOne.textContent = "Loading...";
+  messageTwo.textContent = "";
+
+  const apiUrl = `http://localhost:3000/weather?address=${location}`;
+  fetch(apiUrl).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        messageOne.textContent = data.error;
+      } else {
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecast;
+      }
+    });
+  });
+});
+
+// weatherForm.addEventListener("submit", async (e) => {
+//   e.preventDefault();
+//   const location = search.value;
+//   const apiUrl = `/weather?address=${location}`;
+//   const weatherInfo = document.querySelector("#weather-info");
+
+//   try {
+//     const response = await fetch(apiUrl);
+//     const data = await response.json();
+
+//     if (data.error) {
+//       weatherInfo.innerHTML = `<p class="error">${data.error}</p>`;
+//     } else {
+//       weatherInfo.innerHTML = `<p>Location: ${data.location}</p><p>Forecast: ${data.forecast}</p>`;
+//     }
+//   } catch (error) {
+//     weatherInfo.innerHTML = `<p class="error">An error occurred: ${error.message}</p>`;
+//   }
+// });
--- web-server/src/app.js
+++ web-server/src/app.js
@@ -44,7 +44,7 @@ app.get("/weather", (req, res) => {
   const address = req.query.address;
   if (!address) {
     return res.send({
-      error: "You must provide ab address",
+      error: "You must provide an address",
     });
   }
   geocode(address, (error, { latitude, longitude, location } = {}) => {
